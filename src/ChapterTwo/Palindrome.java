fca9f5b8fd6871c1b8fbf0cf40068da4289841bf
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2020-01-15 16:55:18 +0000

    still on chapter 2

---
+++ src/ChapterTwo/Palindrome.java
@@ -0,0 +1,37 @@
+package ChapterTwo;
+
+public class Palindrome {
+    public static void main(String[] args){
+        DoublyLinkedList<Character> tIsPalindrome = new DoublyLinkedList<>();
+        DoublyLinkedList<Character> tNotPalindrome = new DoublyLinkedList<>();
+
+        for (Character c: "maoam".toCharArray()){
+            tIsPalindrome.insertLast(c);
+        }
+
+        for (Character c: "notapalindrome".toCharArray()){
+            tNotPalindrome.insertLast(c);
+        }
+
+        System.out.print("maoam: ");
+        System.out.println(isPalindrome(tIsPalindrome));
+        System.out.print("notapalindrome: ");
+        System.out.println(isPalindrome(tNotPalindrome));
+    }
+
+    public static boolean isPalindrome(DoublyLinkedList<Character> dll){
+        DNode<Character> fromHead = dll.getFirst();
+        DNode<Character> fromTail = dll.getLast();
+
+        while (fromHead.getElement() != dll.getLast().getElement() &&
+        fromTail.getElement() != dll.getFirst().getElement()){
+            if (fromHead.getElement() != fromTail.getElement()){
+                return false;
+            }
+            fromHead = fromHead.getNext();
+            fromTail = fromTail.getPrev();
+        }
+
+        return true;
+    }
+}

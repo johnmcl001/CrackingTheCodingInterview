ea836428486d35bcfe0dcccc44df5d489a901041
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2020-01-24 15:48:35 +0000

    chapter three, stack of plates completed

---
+++ src/chapterthree/StackOfPlates.java
@@ -0,0 +1,132 @@
+package chapterthree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class StackOfPlates<E> {
+    public static void main(String[] args){
+        StackOfPlates<Integer> stacks = new StackOfPlates<>(10);
+
+        for (int i = 0; i < 35; i++){
+            stacks.push(i);
+        }
+
+        System.out.println(stacks.numberOfStacks());
+
+        System.out.println(stacks);
+        System.out.println();
+        System.out.println(stacks.popAt(2));
+
+        System.out.println(stacks);
+
+        for (int i = 0; i < 10; i++){
+            stacks.popAt(0);
+        }
+
+        System.out.println();
+        System.out.println(stacks);
+
+        System.out.println(stacks.currentStack);
+
+        while(stacks.numberOfStacks() == 3){
+            stacks.popAt(2);
+        }
+
+        System.out.println();
+        System.out.println(stacks.currentStack);
+    }
+
+    // Attributes
+    Vector<Stack<E>> stacks = new Vector<>();
+    Stack<E> currentStack;
+    private final int CAPACITY;
+
+    // Constructor
+    public StackOfPlates(int c){
+        currentStack = new Stack<>();
+        stacks.add(currentStack);
+        CAPACITY = c;
+    }
+
+    // Getters
+
+    /**
+     * return number of stacks present
+     * @return int, number of stacks in stacks of platesj
+     */
+    public int numberOfStacks(){
+        return stacks.size();
+    }
+
+    /**
+     * returns top most element of topmost stack without removing it
+     * @return E, topmost element of topmost stack
+     */
+    public E top(){
+        return currentStack.top();
+    }
+
+    // Setters
+
+    /**
+     * pushes new element onto topmost stack, updates current stack if capacity reached
+     * @param e, new element
+     */
+    public void push(E e){
+        currentStack.push(e);
+
+        if (currentStack.getSize() == CAPACITY){
+            currentStack = new Stack<>();
+            stacks.add(currentStack);
+        }
+    }
+
+    /**
+     * removes and returns top of topmost stack, removes stack if empty
+     * @return E, topmost element of topmost stack
+     */
+    public E pop(){
+        E ans = currentStack.pop();
+        if (currentStack.getSize() == 0){
+            stacks.remove(currentStack);
+        }
+        currentStack = stacks.lastElement();
+
+        return ans;
+    }
+
+    /**
+     * returns topmost element of a specific stack, updates stacks if this is emptied
+     * @param i, determines stack to pop from
+     * @return E, topmost element of specified stack
+     */
+    public E popAt(int i){
+        Stack<E> stack = stacks.get(i);
+
+        E ans = stack.pop();
+
+        if (stack.isEmpty()){
+            stacks.remove(stack);
+            if (stack == currentStack) {
+                currentStack = stacks.lastElement();
+            }
+        }
+
+        return ans;
+    }
+
+    // Utilities
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        for (Stack<E> stack: stacks){
+            sb.append(stack.toString());
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
